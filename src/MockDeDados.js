00da56985ac3d7a9bcde86d7af3391eb8a56c5c6
João Vitor Gomes Lara Resende <joaovitorglr@gmail.com>
2022-07-18 22:30:02 -0300

    adicionando valor total do carrinho

--- src/MockDeDados.js
+++ src/MockDeDados.js
@@ -1,8 +1,8 @@
 export const arrayCards = [
-    {id: '1', nome: 'produto1', valor: 'R$ 138,00', img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
-    {id: '2', nome: 'produto2', valor: 'R$ 18,00', img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
-    {id: '3', nome: 'produto3', valor: 'R$ 17,00', img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
-    {id: '4', nome: 'produto4', valor: 'R$ 19,00', img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
-    {id: '5', nome: 'produto5', valor: 'R$ 22,00', img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
-    {id: '6', nome: 'produto6', valor: 'R$ 28,00', img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
+    {id: '1', nome: 'produto1', valor: 138, img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
+    {id: '2', nome: 'produto2', valor: 18, img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
+    {id: '3', nome: 'produto3', valor: 17, img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
+    {id: '4', nome: 'produto4', valor: 19, img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
+    {id: '5', nome: 'produto5', valor: 22, img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
+    {id: '6', nome: 'produto6', valor: 28, img: 'https://img.elo7.com.br/product/main/36F76BB/camiseta-masculina-basica-nelville-streetwear-algodao-100.jpg'},
 ] 
