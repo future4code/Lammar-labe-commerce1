acdcf980346eea67e244dac6e3ac0fbbe3cdbd45
Rafaelquintanilharj <rafaelquintanilha@hotmail.com>
2022-07-23 19:41:59 -0300

    style ajustes finais

--- src/MockDeDados.js
+++ src/MockDeDados.js
@@ -4,12 +4,16 @@ import foguete2 from './img/foguete2.png'
 import foguete3 from './img/foguete3.png'
 import lego from './img/lego.png'
 import robo from './img/robo.png'
+import estacao from './img/estacao.png'
+import gun from './img/gun.png'
 
 export const arrayCards = [
-    {id: '1', nome: 'Space Bot', valor: 138.99, img: arma, desc: 'Robo espacial'},
-    {id: '2', nome: 'Space Capsule', valor: 188.99, img: foguete, desc: 'Capsula espacial c/ piloto '},
-    {id: '3', nome: 'Space Aliens', valor: 179.00, img: foguete2, desc: 'Aliens do filme ToyStory'},
-    {id: '4', nome: 'Space Rover', valor: 199.99, img: foguete3, desc: 'Rover espacial c/ pilotos'},
-    {id: '5', nome: 'Space Battle', valor: 109.50, img: lego, desc: 'Jogo de batalha espacial'},
-    {id: '6', nome: 'Space Car', valor: 288.90, img: robo, desc: 'Carrinho do filme ToyStory'},
+    {id: '1', nome: 'Astro Bot', valor: 138.80, img: arma, desc: 'Robo espacial'},
+    {id: '2', nome: 'Astro Capsule', valor: 188.90, img: foguete, desc: 'Capsula espacial c/ piloto '},
+    {id: '3', nome: 'Astro Aliens', valor: 179.20, img: foguete2, desc: 'Aliens do filme ToyStory'},
+    {id: '4', nome: 'Astro Rover', valor: 199.90, img: foguete3, desc: 'Rover espacial c/ pilotos'},
+    {id: '5', nome: 'Astro Puzzle', valor: 109.50, img: lego, desc: 'Quebra cabeça espacial'},
+    {id: '6', nome: 'Astro Car', valor: 288.90, img: robo, desc: 'Carrinho do filme ToyStory'},
+    {id: '7', nome: 'Astro Station', valor: 238.90, img: estacao, desc: 'Estação espacial montável'},
+    {id: '8', nome: 'Astro Gun', valor: 338.60, img: gun, desc: 'Arma espacial c/ laser'},
 ] 
